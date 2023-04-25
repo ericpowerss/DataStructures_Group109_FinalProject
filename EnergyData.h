9374c4fdf91af6d0a2c21a04f22900b30c15c20b
RaceEisman <131042485+RaceEisman@users.noreply.github.com>
2023-04-25 17:41:29 -0400

    Add files via upload

--- EnergyData.h
+++ EnergyData.h
@@ -4,13 +4,18 @@
 
 #include <string>
 #include <unordered_map>
+#include <map>
 #include "State.h"
 
 using namespace std;
 
 class EnergyData {
-
+public:
     unordered_map<string, unordered_map<int, State>> energyData;
+    unordered_map<string, unordered_map<int, State>>::iterator it;
+    unordered_map<int, State>::iterator iter;
+        //State name,  energy type,     data,
+    map<string, map<string, vector<pair<int, int>>>> sortedData;
 
 };
 
