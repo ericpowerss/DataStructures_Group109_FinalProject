6f98e4cdf25a4394e451ce202078cd508a9aee52
Remingtonewing <rewing@ufl.edu>
2023-04-25 22:00:17 -0400

    Fixed values and updated vector values

--- EnergyData.h
+++ EnergyData.h
@@ -1,18 +1,20 @@
 
-#ifndef DATASTRUCTURES_GROUP109_FINALPROJECT_ENERGYDATA_H
-#define DATASTRUCTURES_GROUP109_FINALPROJECT_ENERGYDATA_H
-
 #include <string>
 #include <unordered_map>
+#include <map>
 #include "State.h"
+#include <vector>
 
 using namespace std;
 
 class EnergyData {
-
+public:
     unordered_map<string, unordered_map<int, State>> energyData;
+    unordered_map<string, unordered_map<int, State>>::iterator it;
+    unordered_map<int, State>::iterator iter;
+    //State name,  energy type,     data, year
+    map<string, map<string, vector<pair<int, int>>>> sortedData;
 
 };
 
 
-#endif //DATASTRUCTURES_GROUP109_FINALPROJECT_ENERGYDATA_H
