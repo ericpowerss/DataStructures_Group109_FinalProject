f34f8c85523450ed68450cc0e48d584473ad598b
RaceEisman <131042485+RaceEisman@users.noreply.github.com>
2023-04-25 23:19:39 -0400

    Add files via upload

--- EnergyData.h
+++ EnergyData.h
@@ -1,9 +1,11 @@
 
+#ifndef DATASTRUCTURES_GROUP109_FINALPROJECT_ENERGYDATA_H
+#define DATASTRUCTURES_GROUP109_FINALPROJECT_ENERGYDATA_H
+
 #include <string>
 #include <unordered_map>
 #include <map>
 #include "State.h"
-#include <vector>
 
 using namespace std;
 
@@ -12,9 +14,9 @@ public:
     unordered_map<string, unordered_map<int, State>> energyData;
     unordered_map<string, unordered_map<int, State>>::iterator it;
     unordered_map<int, State>::iterator iter;
-    //State name,  energy type,     data, year
-    map<string, map<string, vector<pair<int, int>>>> sortedData;
 
+    map<string, map<string, vector<pair<int, float>>>> sortedData;
 };
 
 
+#endif //DATASTRUCTURES_GROUP109_FINALPROJECT_ENERGYDATA_H
